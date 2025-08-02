0624c3af095df35416b30fbf391c6f0c873c2c9d
Vidushi Krishna <vidushikris@gmail.com>
2025-08-02 14:25:17 +0800

    Initial Commit

---
+++ bobby/bobbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  bobbyApp.swift
+//  bobby
+//
+//  Created by Vidushi Krishna on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct bobbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
